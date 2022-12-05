bf8cf90a067a7bbd0ba79e4ec1608743fdda7155
Saurabh Jumnalkar <saurabhjum@gmail.com>
2022-12-05 22:57:13 +0530

    Update

--- app.py
+++ app.py
@@ -1,65 +1,3 @@
-# from flask import Flask, request, jsonify, render_template
-# import pandas as pd
-# import numpy as np
-# import joblib
-# import traceback
-
-# app=Flask(__name__)
-
-# model=joblib.load("model.pkl")
-# print("model loaded")
-
-# data_columns=joblib.load("data_columns.pkl")
-# print("columns loaded")
-
-# model_cols=joblib.load("data_cols.pkl")
-# print("columns loaded")
-
-# @app.route("/")
-# def hello():
-# 	return render_template("home.html")
-
-# @app.route("/api", methods=["GET","POST"])
-# def predict_mush():
-# 	if model:
-# 		try:
-# 			json_ = request.json
-# 			querry=pd.get_dummies(pd.DataFrame(json_))
-# 			querry=querry.reindex(columns=model_cols , fill_value=0)
-# 			prediction= int(model.predict(querry))
-# 			# return jsonify({"Prediction ": prediction})
-# 			if prediction == 0:
-# 				return jsonify({"Prediction ": "Poisonous"})
-# 			else:
-# 				return jsonify({"Prediction ": "Non-Poisonous"})
-
-
-
-# 		except:
-# 			return jsonify({"trace ": traceback.format_exc()})
-# 	else:
-# 		print("first train the model")
-# 		return ("no model is here to use")
-
-
-# @app.route("/predict" , methods=["GET","POST"])
-# def predict():
-# 	data= [str(i) for i in request.form.values()]
-# 	print(data)
-# 	input = pd.DataFrame(np.array(data).reshape(1,-1), columns=  data_columns)
-# 	final_input= pd.get_dummies(input)
-# 	final= final_input.reindex(columns=model_cols, fill_value=0)
-# 	output = model.predict(final)[0]
-# 	if output == 0:
-# 		return render_template('home.html', mushroom ="The mushroom is Poisonous.")
-# 	else:
-# 		return render_template('home.html', mushroom ="The mushroom is Edible.")
-
-	
-
-
-# if __name__ == "__main__":
-# 	app.run(debug= True)
 
 from flask import Flask, render_template, request, url_for, redirect, session,jsonify
 import pymongo
@@ -67,22 +5,44 @@ import bcrypt
 import pandas as pd
 import numpy as np
 import joblib
+from logger.logger import Logs
+
+
+
+log = Logs("logs.log")
+log.addLog("INFO", "Execution started Successfully !")
+
+
 
 app = Flask(__name__)
 app.secret_key = "testing"
-client = pymongo.MongoClient("mongodb+srv://sourabh108:Pass123@cluster0.bp2fo.mongodb.net/?retryWrites=true&w=majority")
-db = client.get_database('Mushroom')
-records = db.register
+
+try:
+    Userkey="sourabh108:Pass123" 
+    mongodbcompassURlconnection="mongodb+srv://"+Userkey+"@cluster0.bp2fo.mongodb.net/?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(mongodbcompassURlconnection)
+except Exception as error:
+    print("mongodb client connection error")
+    log.addLog("ERROR","Error while connecting client")
+
+try:
+    db = client.get_database('Mushroom')
+    records = db.register
+except Exception as error:
+    print("Error while getting/creating database")
+    log.addLog("ERROR","Error while getting/creating database")
+
+
 
 
 model=joblib.load("model.pkl")
-print("model loaded")
+log.addLog("INFO","model loaded")
 
 data_columns=joblib.load("data_columns.pkl")
-print("columns loaded")
+log.addLog("INFO","columns loaded")
 
 model_cols=joblib.load("data_cols.pkl")
-print("columns loaded")
+log.addLog("INFO","columns loaded")
 
 @app.route('/')
 def index():
@@ -115,6 +75,8 @@ def login():
             
             if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
                 session["email"] = email_val
+                log.addLog("INFO","Login Successful")
+
                 return redirect(url_for('dashboard'))
             else:
                 if "email" in session:
@@ -137,7 +99,7 @@ def signin():
         email = request.form.get("email")
 
         password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
+        recheck_password = request.form.get("recheck_password")
 
         user_found = records.find_one({"name": user})
         email_found = records.find_one({"email": email})
@@ -150,17 +112,19 @@ def signin():
             message = 'This email already exists in database'
             return render_template('signin.html',message = message)
 
-        if password1 != password2:
+        if password1 != recheck_password:
             message = 'Passwords should match!'
             return render_template('signin.html',message = message)
         else:
-            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            hashed = bcrypt.hashpw(recheck_password.encode('utf-8'), bcrypt.gensalt())
             user_input = {'name': user, 'email': email, 'password': hashed}
             records.insert_one(user_input)
             
             user_data = records.find_one({"email": email})
             new_email = user_data['email']
             session["email"] = new_email
+            log.addLog("INFO","Signin Successful")
+
             if "email" in session:
                     return redirect(url_for("dashboard"))
             return render_template('dashboard.html',email = new_email)    
@@ -171,11 +135,12 @@ def predict():
     if "email" in session:
         email = session["email"]
         data= [str(i) for i in request.form.values()]
-        print(data)
         input = pd.DataFrame(np.array(data).reshape(1,-1), columns=  data_columns)
         final_input= pd.get_dummies(input)
         final= final_input.reindex(columns=model_cols, fill_value=0)
+        log.addLog("INFO","Model is running")
         output = model.predict(final)[0]
+        log.addLog("INFO","Model given output")
         if output == 0:
             return render_template('dashboard.html', mushroom ="The mushroom is Poisonous.",email = email)
         else:
@@ -189,9 +154,10 @@ def predict():
 def logout():
     if "email" in session:
         session.pop("email", None)
+        log.addLog("INFO","Logout successful")
         return render_template("login.html")
     else:
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
